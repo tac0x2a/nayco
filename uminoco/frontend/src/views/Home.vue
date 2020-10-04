cdddaabbdfef80bef9998d2c1b4731eb2c5013db
TAC <tac@tac42.net>
2020-10-04 20:50:20 +0900

    Add DataSourceManager card

--- uminoco/frontend/src/views/Home.vue
+++ uminoco/frontend/src/views/Home.vue
@@ -18,13 +18,13 @@
           <v-card class="mx-auto">
             <v-card-text>
               <v-icon>mdi-table-large</v-icon>
-              <p class="display-1 text--primary">Manage Tables</p>
+              <p class="display-1 text--primary">Tables Manager</p>
               <div class="text--primary">
                 You can rename and delete tables, see what storage and compression rates are being used.
               </div>
             </v-card-text>
             <v-card-actions>
-              <v-btn text color="indigo accent-4" to="/table">Open Tables</v-btn>
+              <v-btn text color="indigo accent-4" to="/table">Open</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -32,15 +32,31 @@
         <v-col key="1" cols="12" sm="4">
           <v-card class="mx-auto">
             <v-card-text>
-              <v-icon>mdi-table-large</v-icon>
-              <p class="display-1 text--primary">Migration Table Tool</p>
+              <v-icon>mdi-call-merge</v-icon>
+              <p class="display-1 text--primary">Migration Tables</p>
               <div class="text--primary">
                 You can copy table data into other table.
                 This feature is expected to use when created new table by receiving schema similer to old one. You can integrate data into new table.
               </div>
             </v-card-text>
             <v-card-actions>
-              <v-btn text color="indigo accent-4" to="/table_migration">Open Migration Table Tool</v-btn>
+              <v-btn text color="indigo accent-4" to="/table_migration">Open</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col key="1" cols="12" sm="4">
+          <v-card class="mx-auto">
+            <v-card-text>
+              <v-icon>mdi-water</v-icon>
+              <p class="display-1 text--primary">Data Source Manager</p>
+              <div class="text--primary">
+                View the List fo data sources.
+                You can edit the specified types to correct data type on Grebe.
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="indigo accent-4" to="/source">Open</v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
